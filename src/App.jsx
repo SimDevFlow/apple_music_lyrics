23d33f56b7531b50b6d3cd4951e21fc4d1eb1288
Simdev <simonetoa04@gmail.com>
2025-03-14 11:58:27 +0100

    adding auto scroll and opacity fade out

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,44 @@
-import { useState } from 'react'
+import { useRef, useState, useEffect } from 'react'
 import { texts } from './lyrics'
 import LyricsTitle from './components/LyricsTitle'
 
 
 function App() {
 
+  const scrollRef = useRef(null);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (scrollRef.current) {
+        scrollRef.current.scrollBy({
+          top: 160,
+          behavior: 'smooth',
+        });
+      }
+    }, 1000);
+
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
-    <div className='w-screen h-screen flex overflow-hidden text-white'>
-      <div className='flex-1/2 bg-amber-300 p-8'><img src="/taylor.jpg" className='rounded-2xl mx-auto font-apple ' width="400px" alt="taylor" />
+    <div className='w-screen h-screen flex overflow-hidden text-white bg-linear-to-br from-[#6B94B2]  to-[#C79676]'>
+      <div className='flex-1/2 p-8'><img src="/taylor.jpg" className='rounded-2xl mx-auto font-apple mb-2' width="400px" alt="taylor" />
+        <div className='w-full max-w-[364px] flex gap-2 items-center text-xs mx-auto font-apple font-semibold'>
+          <p>0:50</p>
+          <div className='w-full h-2.5 rounded-full overflow-clip relative '>
+            <div className='h-full w-full bg-white opacity-50 rounded-es-full-full absolute'></div>
+            <div className='h-full w-8 bg-white opacity-100 rounded-es-full-full absolute z-10'></div>
+          </div>
+          <p>-2:56</p>
+        </div>
         <p className=' font-bold  text-2xl text-center'>This Love(Taylor's Version)</p>
         <p className='text-center font-semibold'>Taylor Swift</p></div>
-      <div className='pt-64 flex-1/2 bg-blue-400 gt p-8 overflow-y-scroll no-scrollbar'>
+      <div ref={scrollRef} className='pt-64 flex-1/2 gt p-8 overflow-y-scroll no-scrollbar'>
         {
-          texts.map((text, index) => <LyricsTitle key={index}>{text}</LyricsTitle>)
+          texts.map((text, index) => <LyricsTitle key={index} id={text.id}>{text.lyric}</LyricsTitle>)
         }
+        <div className='h-32'></div>
       </div>
     </div>
   )
--- src/components/LyricsTitle.jsx
+++ src/components/LyricsTitle.jsx
@@ -1,15 +1,45 @@
-import React, { useEffect, useRef } from 'react'
-import { motion, useInView } from 'motion/react'
+import React, { useEffect, useRef, useState } from 'react'
+import { motion, useAnimate, useInView } from 'motion/react'
+import { useLyricStore } from '../store'
+import classNames from 'classnames'
 
-function LyricsTitle({children,isActive}) {
-    const ref = useRef(null)
-    const inView = useInView(ref,{margin:"-256px 0px 0px 0px"})
-    useEffect(()=>{
-        console.log(inView)
-    },[inView])
-  return (
-    <motion.p ref={ref} className='text-4xl font-bold font-apple mb-16 opacity-70'>{children}</motion.p>
-  )
+function LyricsTitle({ children, id }) {
+    // const ref = useRef(null)
+    const setInViewLyric = useLyricStore((state) => state.setActiveLyric)
+    const inViewLyric = useLyricStore((state) => state.activeLyric)
+    const [positionY, setPositionY] = useState()
+    const [scope, animate] = useAnimate()
+    const [opacity,setOpacity] = useState(.5)
+    const inView = useInView(scope, { margin: "-250px 0px -290px 0px", })
+
+    useEffect(() => {
+
+        if (inView){ 
+            setInViewLyric(id)
+            setOpacity(1)
+        }else{
+            setOpacity(.5)
+        }
+        if (!inView && inViewLyric == id) setInViewLyric(null)
+        if (scope.current) {
+            setPositionY(scope.current.getBoundingClientRect().top);
+        }
+
+    }, [inView, id, setInViewLyric])
+
+    return (
+
+        < div >
+            {
+                <motion.p
+                    initial={{ opacity: 1,y:0 }}
+                    animate={{y:inView?[-10,0] : 0,opacity:(positionY<220 && !inView)?0:opacity,transition:{duration:.5}}}
+                    ref={scope} className={classNames('text-3xl font-bold font-apple mb-24 leading-8 opacity-5')}>{children}</motion.p>
+            }
+        </div >
+
+
+    )
 }
 
 export default LyricsTitle
